4ab0ca06a3a958fe20045298d46b578ebca1ed3b
Dr G Green <eyalimya@gmail.com>
2024-08-09 21:37:03 +0300

    dammage test 1

--- Assets/Scripts/Weapons/Rapier.cs
+++ Assets/Scripts/Weapons/Rapier.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Rapier : MonoBehaviour
@@ -7,6 +8,17 @@ public class Rapier : MonoBehaviour
     
     public void Attack()
     {
-        RaycastHit hit;
+        RaycastHit hitEnemy;
+        Physics.Raycast(transform.position, Vector3.forward, out hitEnemy, 5f);
+
+            hitEnemy.transform.gameObject.GetComponent<BaseEnemy>()?.TakeDammage(10);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack();
+        }
     }
 }
