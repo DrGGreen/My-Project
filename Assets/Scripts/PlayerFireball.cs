ea000317df7a80823c85f6faaf272d0a3727a383
Boonki1 <etai.melam@gmail.com>
2024-08-11 17:16:47 +0300

    Revert "You can shoot fire now by right clicking"
    This reverts commit aa8352c906a43c56a5075915b6dd4773fb562e94.

--- Assets/Scripts/PlayerFireball.cs
+++ Assets/Scripts/PlayerFireball.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlayerFireball : MonoBehaviour
 {
-    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire2")){
-            Instantiate(prefab, transform.position, transform.rotation);
-        }
+        
     }
 }
